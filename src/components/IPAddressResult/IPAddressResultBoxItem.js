32c0a99170814b2c2263f04ff9e88deeaa8774a9
davidolaoluwa240 <davidolaoluwa240@gmail.com>
2022-07-21 10:19:17 -0300

    center loading spinner on tablet portrait and mobile screen

--- src/components/IPAddressResult/IPAddressResultBoxItem.js
+++ src/components/IPAddressResult/IPAddressResultBoxItem.js
@@ -9,12 +9,14 @@ const IPAddressResultBoxItem = ({ heading, content, loading }) => {
     <div className="search-result-box-item">
       <h3 className="search-result-box-item__heading">{heading}</h3>
       {loading && (
-        <ThreeDots
-          height="20"
-          width="50"
-          color="hsl(0, 0%, 59%)"
-          ariaLabel="loading"
-        />
+        <div className="search-result-box-item__loader">
+          <ThreeDots
+            height="20"
+            width="50"
+            color="hsl(0, 0%, 59%)"
+            ariaLabel="loading"
+          />
+        </div>
       )}
       {!loading && <p className="search-result-box-item__content">{content}</p>}
     </div>
